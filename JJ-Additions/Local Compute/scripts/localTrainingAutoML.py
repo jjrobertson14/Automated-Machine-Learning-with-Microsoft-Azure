60545feff14466fa8b20912ae53521db46776ab4
jjrobertson14 <johnathon.j.robertson@gmail.com>
2022-04-25 23:04:59 -0400

    Model Explanation generated successfully, not visualized or accessible in AMLS

--- JJ-Additions/Local Compute/scripts/localTrainingAutoML.py
+++ JJ-Additions/Local Compute/scripts/localTrainingAutoML.py
@@ -1,6 +1,7 @@
 from azureml.core import Workspace, Dataset, Datastore
 from azureml.core.authentication import InteractiveLoginAuthentication
 from azureml.core.run import Run
+from azureml.interpret import ExplanationClient
 
 import numpy as np
 import pandas as pd
@@ -73,6 +74,7 @@ ws = Workspace.get(name=ws_name,
 print('ws.name:', ws.name, 'ws.resource_group:', ws.resource_group, 'ws.location:', ws.location, 'ws.subscription_id:', ws.subscription_id, sep='\n')
 
 run = Run.get_context()
+client = ExplanationClient.from_run(run)
 run.log(name='creating outputs and logs directory...', value=0)
 run.log(os.makedirs('./outputs', exist_ok=True), value=0)
 run.log(os.makedirs('./outputs', exist_ok=True), value=0)
@@ -90,7 +92,11 @@ df = dataset.to_pandas_dataframe()
 df_column_names = ['Survived', 'Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Cabin', 'Embarked']
 df_numeric_column_names = ['Age', 'Fare']
 
+
+
+
 # BEGIN Reform df dataframe so that it contains all numbers that are preprocessed...
+
 # For int column Age, Impute NaN numeric values, and Remove outliers
 print('Before Removing outliers or Imputing null values, df[Age]: ', df['Age'])
 ageMedian = np.nanmedian(df['Age'])
@@ -111,6 +117,7 @@ print('After Removing outliers and Imputing null values, df[Age]: ', df['Age'])
 # Copy df, keeping only Age column, set type of this df copy to float
 df_age_column = pd.DataFrame(df['Age'], columns=['Age'])
 
+
 # Copy df, keeping only float numeric columns, set type of this df copy to float
 df_float_column_names = ['Fare']
 print('df_float_column_names: ', df_float_column_names)
@@ -121,6 +128,7 @@ print('Before concatenation to df_numeric_columns, df[Age]: ', df['Age'])
 print('Before concatenation to df_numeric_columns, df_age_column: ', df_age_column)
 df_numeric_columns = pd.concat([df_age_column, df_float_columns], keys=df_numeric_column_names, axis=1)
 print('concatenated df_numeric_columns: ', df_numeric_columns)
+
 
 # Use StandardScaler or MinMaxScaler on Numeric/Non-Categorical columns split
 scaler = StandardScaler().fit(df_numeric_columns)
@@ -141,17 +149,13 @@ df_categorical_columns = pd.DataFrame(df[df_categorical_column_names], dtype=np.
 print('df_categorical_columns: ', df_categorical_columns)
 encoder = OneHotEncoder(drop='first', handle_unknown='error', sparse=False, ).fit(df_categorical_columns)
 print('encoder.categories: ', encoder.categories)
-# Should get something similiar in form to this printed...
-#   [array(['female', 'male'], dtype=object), array(['from Europe', 'from US'], dtype=object), array(['uses Firefox', 'uses Safari'], dtype=object)]
 df_encoded_categorical_columns = pd.DataFrame(encoder.transform(df_categorical_columns))
 df_encoded_categorical_columns.columns = encoder.get_feature_names(df_categorical_column_names)
 print('df_encoded_categorical_columns: ', df_encoded_categorical_columns)
-# Should get something like this returned...
-#   array([[1., 0., 0., 1., 0., 1.],
-#     [0., 1., 1., 0., 0., 1.]])
 #   By default, the values each feature can take is inferred automatically from the dataset and can be found in the categories_ attribute:
-# Combine the numeric DF with the categorical DF
 
+
+# Combine the numeric DF with the categorical DF
 dfs = [df['Survived'], df_scaled_numeric_columns, df_encoded_categorical_columns]
 print('Before concatenation to dfTyped, df[\'Survived\']: ', df['Survived'])
 print('Before concatenation to dfTyped, df[Age]: ', df['Age'])
@@ -161,7 +165,7 @@ dfTyped = pd.concat(dfs, axis=1)
 print('dfTyped: ', dfTyped)
 print('dfTyped[Age]: ', dfTyped['Age'])
 
-        # - ! With sklearn.preprocessing, preprocess your Dataframes before training model in the Python Script
+        # - With sklearn.preprocessing, preprocess your Dataframes before training model in the Python Script
         #     - [Guide at SciKit Learn site](https://scikit-learn.org/stable/modules/preprocessing.html)
         #     - Use OneHotEncoder
         #     - Use StandardScaler or  MinMaxScaler while you're at it
@@ -174,7 +178,11 @@ print('dfTyped[Age]: ', dfTyped['Age'])
 # Initial Data Frame is now preprocessed in dfPreprocessed
 dfPreprocessed = dfTyped
 print('dfPreprocessed: ', dfPreprocessed)
+
 # END Reform df dataframe so that it contains all numbers...
+
+
+
 
 # Split DataFrame for training now that it is pre-processed
 target_column_name = 'Survived'
@@ -196,21 +204,26 @@ data = {"train": {"X": X_train, "y": y_train},
 
 # Training the model is as simple as this
 # Use the function imported above and apply fit() on it
-DT = DecisionTreeClassifier()
-DT.fit(X_train,y_train)
+model_DT = DecisionTreeClassifier()
+model_DT.fit(X_train,y_train)
 
 # We use the predict() on the model to predict the output
-prediction = DT.predict(X_test)
+prediction = model_DT.predict(X_test)
 
 # For classification, using accuracy and F1 score
 print(accuracy_score(y_test,prediction))
 print(f1_score(y_test,prediction))
 
+
+
 # Save the output model to a file...
 # ... This is required for the model to get automatically uploaded by the Notebook using this script
 with open(p_out_model_file_name, "wb") as file:
-    joblib.dump(value=DT, filename=os.path.join('./outputs/', p_out_model_file_name))
+    joblib.dump(value=model_DT, filename=os.path.join('./outputs/', p_out_model_file_name))
 file.close()
+
+
+
 
 # for regression...
 #
@@ -221,5 +234,51 @@ file.close()
 # from sklearn.metrics import r2_score
 # print(mean_absolute_error(y_test,prediction))
 # print(r2_score(y_test,prediction))
+
+
+
+
+# BEGIN Add Explanations
+from interpret.ext.blackbox import TabularExplainer
+
+# "features" and "classes" fields are optional
+explainer = TabularExplainer(model_DT, 
+                             X_train, 
+                             features=[*df_numeric_column_names, *df_encoded_categorical_columns.columns])
+
+# Get Global Explanations, global as in 'of total data'...
+
+# you can use the training data or the test data here, but test data would allow you to use Explanation Exploration
+global_explanation = explainer.explain_global(X_test)
+# if you used the PFIExplainer in the previous step, use the next line of code instead
+# globa l_explanation = explainer.explain_global(x_train, true_labels=y_train)
+# sorted feature importance values and feature names
+sorted_global_importance_values = global_explanation.get_ranked_global_values()
+sorted_global_importance_names = global_explanation.get_ranked_global_names()
+globalFeatureExplanations = dict(zip(sorted_global_importance_names, sorted_global_importance_values))
+print('globalFeatureExplanations: ', globalFeatureExplanations)
+# alternatively, you can print out a dictionary that holds the top K feature names and values
+print('global_explanation.get_feature_importance_dict(): ', global_explanation.get_feature_importance_dict())
+
+# Uploading global model explanation data...
+
+# the explanation can then be downloaded on any compute
+# multiple explanations can be uploaded
+client.upload_model_explanation(global_explanation, comment='global explanation: all features')
+# or you can only upload the explanation object with the top k feature info
+#client.upload_model_explanation(global_explanation, top_k=2, comment='global explanation: Only top 2 features')
+
+# Get Local Explanation, local as in 'of specified part of data'...
+
+# get explanation for the first few data points in the test set
+local_explanation = explainer.explain_local(X_test[0:5])
+# sorted feature importance values and feature names
+sorted_local_importance_names = local_explanation.get_ranked_local_names()
+print('sorted_local_importance_names: ', sorted_local_importance_names)
+sorted_local_importance_values = local_explanation.get_ranked_local_values()
+print('sorted_local_importance_values: ', sorted_local_importance_values)
+# Warning, there was an error when I ran this, and I left print statements to help
+# localFeatureExplanations = dict(zip(sorted_local_importance_names, sorted_local_importance_values))
+# print('localFeatureExplanations: ', localFeatureExplanations)
 
 exit(0)
